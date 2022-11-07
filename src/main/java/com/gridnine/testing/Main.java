2ef648e57a978b5ca4e7f154d52bf231917f7797
Александр Вовк <avovk@ubic.tech>
2022-11-07 08:11:20 +0300

    swear

--- src/main/java/com/gridnine/testing/Main.java
+++ src/main/java/com/gridnine/testing/Main.java
@@ -1,4 +1,10 @@
 package com.gridnine.testing;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOError;
+import java.io.IOException;
+
 /**
  * Main.
  * @author Vovk Alexander (vovk.ag747@gmail.com)
@@ -12,6 +18,15 @@ public class Main {
         filter.untilCurrentTime(FlightBuilder.createFlights());
         filter.arrivalEarlierThanFlyout(FlightBuilder.createFlights());
         filter.timeOnGroundMoreTwoHours(FlightBuilder.createFlights());
+        try(BufferedReader reader = new BufferedReader(new FileReader("icons/swear.txt"))) {
+         String line;
+         while ((line = reader.readLine()) != null) {
+             System.out.println(line);
+         }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 
